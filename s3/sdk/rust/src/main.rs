5a9a68d46d3a56a090242f8c1f8fd21b2cff4ad8
nilton <ngcquimica@gmail.com>
2025-10-11 06:14:46 -0300

    create bucket with rust sdk

---
+++ s3/sdk/rust/src/main.rs
@@ -0,0 +1,57 @@
+use clap::Parser;
+
+use aws_sdk_s3::{Client, Error};
+use aws_config::BehaviorVersion;
+
+//https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/dotnetv3/S3/S3_Basics/S3Bucket.cs#L12
+//https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/rustv1/examples/s3/src/lib.rs
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    bucket_name_arg: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let bucket_name = &args.bucket_name_arg;
+
+    // Carrega config padrão do AWS CLI
+    let shared_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let region = shared_config.region().unwrap();
+    println!("🌍 region load: {}", region);
+
+    let client = Client::new(&shared_config);
+
+    // Cria o bucket
+    create_bucket(&client, bucket_name).await?;
+
+    println!("✅ Bucket created: {}", bucket_name);
+    Ok(())
+}
+
+pub async fn create_bucket(
+    client: &aws_sdk_s3::Client,
+    bucket_name: &str,
+) -> Result<Option<aws_sdk_s3::operation::create_bucket::CreateBucketOutput>, Error> {
+    
+    let create = client
+        .create_bucket()
+        .bucket(bucket_name)
+        .send()
+        .await;
+
+    // BucketAlreadyExists and BucketAlreadyOwnedByYou are not problems for this task.
+    create.map(Some).or_else(|err| {
+        if err
+            .as_service_error()
+            .map(|se| se.is_bucket_already_exists() || se.is_bucket_already_owned_by_you())
+            == Some(true)
+        {
+            Ok(None)
+        } else {
+            Err(Error::from(err))
+        }
+    })
+}
